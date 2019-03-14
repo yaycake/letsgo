7fd1c4db866186e74eccbaafe40ff48a4259f3ff
Grace Yang <yaycake@gmail.com>
2019-03-14 21:25:13 +0800

    api key for parks

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
-import num from './test';
+import park from './models/Park';
+import * as parkView from './views/parkView';
+import axios from 'axios';
 
-const x = 23;
+// National Parks Service API Key:
+// LMCWhYBZ0nL2NEDRcXJGidsSEZRyzLxfu19EiQSE
 
-console.log(`Imported ${num} from test module. variable x is ${x}`);
+
+async function getResult(query) {
+  const baseUrl = 'http://api.nps.gov/api/v1/'
+
+  const apiKey = 'LMCWhYBZ0nL2NEDRcXJGidsSEZRyzLxfu19EiQSE'
+
+  const res = await axios(`${baseUrl}parks?parkCode=${query}&api_key=${apiKey}`);
+
+  console.log(res)
+}
+
+console.log(getResult('acad'));
